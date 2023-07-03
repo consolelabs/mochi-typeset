7dc8263f82f938301f87a056591813747f9874e7
huynguyenh <hoanghuy2908@gmail.com>
2023-07-03 15:19:46 +0700

    chore: add comment for notification

--- typeset/notification.go
+++ typeset/notification.go
@@ -2,12 +2,20 @@ package typeset
 
 // Enum used to identify the type of notification
 const (
+	// NOTIFICATION_BINANCE_CONNECT: Notification when user connects to Binance
 	NOTIFICATION_BINANCE_CONNECT = iota
+	// NOTIFICATION_BINANCE_DISCONNECT: later
 	NOTIFICATION_INTEGRATION_START
+	// NOTIFICATION_INTEGRATION_FINISH: later
 	NOTIFICATION_INTEGRATION_FINISH
+	// NOTIFICATION_PAY_WITHDRAW: Notification when user withdraws fund from mochi
 	NOTIFICATION_PAY_WITHDRAW
+	// NOTIFICATION_PAY_DEPOSIT: Notification when user deposits fund to mochi
 	NOTIFICATION_PAY_DEPOSIT
+	// NOTIFICATION_PAY_TRANSFER: Notification when user use swap within mochi
 	NOTIFICATION_SWAP
+	// NOTIFICATION_PAY_TRANSFER: Notification when user using vault
 	NOTIFICATION_VAULT
+	// NOTIFICATION_WALLET_CONNECT: Notification when user connect wallet
 	NOTIFICATION_WALLET_CONNECT
 )

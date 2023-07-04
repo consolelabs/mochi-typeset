447934b0a9272b8f38dfdaffebc2d6b9dee49a1f
trkhoi <ngotrongkhoi112@gmail.com>
2023-07-04 11:12:50 +0700

    chore: remove number value

--- typeset/notification.go
+++ typeset/notification.go
@@ -7,21 +7,21 @@ const (
 	// NOTIFICATION_BINANCE_CONNECT: Notification when user connects to Binance
 	NOTIFICATION_BINANCE_CONNECT NotificationType = iota
 	// NOTIFICATION_BINANCE_DISCONNECT: later
-	NOTIFICATION_INTEGRATION_START NotificationType = 1
+	NOTIFICATION_INTEGRATION_START
 	// NOTIFICATION_INTEGRATION_FINISH: later
-	NOTIFICATION_INTEGRATION_FINISH NotificationType = 2
+	NOTIFICATION_INTEGRATION_FINISH
 	// NOTIFICATION_PAY_WITHDRAW: Notification when user withdraws fund from mochi
-	NOTIFICATION_PAY_WITHDRAW NotificationType = 3
+	NOTIFICATION_PAY_WITHDRAW
 	// NOTIFICATION_PAY_DEPOSIT: Notification when user deposits fund to mochi
-	NOTIFICATION_PAY_DEPOSIT NotificationType = 4
+	NOTIFICATION_PAY_DEPOSIT
 	// NOTIFICATION_PAY_TRANSFER: Notification when user use swap within mochi
-	NOTIFICATION_SWAP NotificationType = 5
+	NOTIFICATION_SWAP
 	// NOTIFICATION_PAY_TRANSFER: Notification when user using vault
-	NOTIFICATION_VAULT NotificationType = 6
+	NOTIFICATION_VAULT
 	// NOTIFICATION_WALLET_CONNECT: Notification when user connect wallet
-	NOTIFICATION_WALLET_CONNECT NotificationType = 7
+	NOTIFICATION_WALLET_CONNECT
 	// NOTIFICATION_TRIGGER: Notification when user trigger
-	NOTIFICATION_TRIGGER NotificationType = 8
+	NOTIFICATION_TRIGGER
 	// NOTIFICATION_VAULT_PROPOSAL: Notification when user create proposal
 	NOTIFICATION_VAULT_PROPOSAL
 	// NOTIFICATION_VAULT_VOTE: Notification when user vote proposal

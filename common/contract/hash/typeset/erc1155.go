42afd9b24cab0f48bb04dc80a304398019bff4b9
Huy Nguyen Quang <39359294+quanghuynguyen1902@users.noreply.github.com>
2023-07-06 17:33:31 +0700

    feat: update evm event

---
+++ common/contract/hash/typeset/erc1155.go
@@ -0,0 +1,6 @@
+package typeset
+
+// Enum used to identify the hash of erc1155 contract hash
+const (
+	TRANSFER_SINGLE_HASH = "0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62"
+)

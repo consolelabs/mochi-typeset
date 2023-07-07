f33a8c09ca740b835ae8b673b93391500ce937d3
Quang Le <quanglm.ops@gmail.com>
2023-07-07 16:40:01 +0700

    chore: update kfk msg type

--- queue/kafka_message.go
+++ queue/kafka_message.go
@@ -2,19 +2,27 @@ package typeset
 
 import "github.com/consolelabs/mochi-typeset/common/error/typeset"
 
+type KafkaMessageType string
+
+const (
+	KafkaMessageTypeAudit KafkaMessageType = "audit"
+)
+
 type KafkaMessage struct {
-	Type   string `json:"type"`
-	Topic  string `json:"topic"`
-	Data   []byte `json:"message"`
-	Sender string `json:"sender"`
+	Type   KafkaMessageType `json:"type"`
+	Topic  string           `json:"topic"`
+	Data   []byte           `json:"message"`
+	Sender string           `json:"sender"`
 }
 
 // Validate validates the kafka message
 // 1. Type should not be empty
 // 2. Sender should not be empty
 func (k *KafkaMessage) Validate() error {
-	if k.Type == "" {
+	switch k.Type {
+	case KafkaMessageTypeAudit:
+		return nil
+	default:
 		return typeset.ERROR_FAIL_VALIDATION
 	}
-	return nil
 }

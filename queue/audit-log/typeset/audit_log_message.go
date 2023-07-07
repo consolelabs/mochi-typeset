eec4030aa2dd27b1983f5cbf2db93e6d9de6bad5
Quang Le <quanglm.ops@gmail.com>
2023-07-07 11:12:24 +0700

    feat: add audit log type & validate func

--- queue/audit-log/typeset/audit_log_message.go
+++ queue/audit-log/typeset/audit_log_message.go
@@ -1,6 +1,29 @@
 package typeset
 
+import "errors"
+
+type AuditLogType int
+
+const (
+	AuditLogMessageTypeApi AuditLogType = iota
+	AuditLogMessageTypeOnchain
+	AuditLogMessageTypeTwitter
+)
+
 type AuditLogMessage struct {
-	Type   string       `json:"type"`
+	Type   AuditLogType `json:"type"`
 	ApiLog *AuditLogApi `json:"api_log,omitempty"`
+}
+
+func (m *AuditLogMessage) Validate() error {
+	switch m.Type {
+	case AuditLogMessageTypeApi:
+		return m.ApiLog.Validate()
+	case AuditLogMessageTypeOnchain:
+		return nil
+	case AuditLogMessageTypeTwitter:
+		return nil
+	default:
+		return errors.New("invalid audit log message type")
+	}
 }

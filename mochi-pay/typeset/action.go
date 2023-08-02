c475da68f74c08586171d34fd6618c83952a22c3
huynguyenh <hoanghuy2908@gmail.com>
2023-08-02 12:24:34 +0700

    feat: add tx action

---
+++ mochi-pay/typeset/action.go
@@ -0,0 +1,14 @@
+package typeset
+
+type TransactionAction string
+
+const (
+	// TRANSACTION_ACTION_DEPOSIT: Deposit action
+	TRANSACTION_ACTION_DEPOSIT TransactionAction = "deposit"
+	// TRANSACTION_ACTION_WITHDRAW: Withdraw action
+	TRANSACTION_ACTION_WITHDRAW TransactionAction = "withdraw"
+	// TRANSACTION_ACTION_TRANSFER: Transfer action
+	TRANSACTION_ACTION_TRANSFER TransactionAction = "transfer"
+	// TRANSACTION_ACTION_SWAP: Swap action
+	TRANSACTION_ACTION_SWAP TransactionAction = "swap"
+)

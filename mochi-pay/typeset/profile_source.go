8c9b6bf6d8f3062117920adb82d5decdb16fbc79
Anh Nguyen <anhnh@dwarvesv.com>
2023-08-11 20:52:53 +0700

    chore: update profile tx

---
+++ mochi-pay/typeset/profile_source.go
@@ -0,0 +1,8 @@
+package typeset
+
+type TransactionProfileSource string
+
+const (
+	// TRANSACTION_PROFILE_SOURCE_MOCHI_BALANCE: Mochi's profile balance
+	TRANSACTION_PROFILE_SOURCE_MOCHI_BALANCE TransactionProfileSource = "mochi-balance"
+)

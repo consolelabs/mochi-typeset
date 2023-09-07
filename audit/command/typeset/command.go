1e72e0b55a23cd58f140a79f2de6ce4d836df0c4
Huy Nguyen Quang <39359294+quanghuynguyen1902@users.noreply.github.com>
2023-09-07 21:51:08 +0700

    feat: add command typeset

---
+++ audit/command/typeset/command.go
@@ -0,0 +1,12 @@
+package typeset
+
+import "time"
+
+type ProfileCommandUsage struct {
+	ProfileId string    `json:"profile_id"`
+	Command   string    `json:"command"`
+	Params    string    `json:"params"`
+	Platform  string    `json:"platform"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}

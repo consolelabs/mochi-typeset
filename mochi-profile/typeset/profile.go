75bf8cb5b6deb6a876c5784e4a8fc2db7da77da2
Anh Nguyen <nguyenhoanganh0772@gmail.com>
2024-03-08 17:20:28 +0700

    chore: add profile type - application_vault

--- mochi-profile/typeset/profile.go
+++ mochi-profile/typeset/profile.go
@@ -3,7 +3,8 @@ package typeset
 type ProfileType string
 
 const (
-	PROFILE_TYPE_USER        ProfileType = "user"
-	PROFILE_TYPE_APPLICATION ProfileType = "application"
-	PROFILE_TYPE_VAULT       ProfileType = "vault"
+	PROFILE_TYPE_USER              ProfileType = "user"
+	PROFILE_TYPE_APPLICATION       ProfileType = "application"
+	PROFILE_TYPE_VAULT             ProfileType = "vault"
+	PROFILE_TYPE_APPLICATION_VAULT ProfileType = "application_vault"
 )

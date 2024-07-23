71c3c33d85034ffcb5e80027fce5bf136c3c5b3d
Anh Nguyen <nguyenhoanganh0772@gmail.com>
2024-07-23 17:39:20 +0700

    chore: support earning vault profile

--- mochi-profile/typeset/profile.go
+++ mochi-profile/typeset/profile.go
@@ -7,4 +7,5 @@ const (
 	PROFILE_TYPE_APPLICATION       ProfileType = "application"
 	PROFILE_TYPE_VAULT             ProfileType = "vault"
 	PROFILE_TYPE_APPLICATION_VAULT ProfileType = "application_vault"
+	PROFILE_TYPE_EARNING_VAULT     ProfileType = "earning_vault"
 )
